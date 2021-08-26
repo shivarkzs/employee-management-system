41ef08f35b31177510233283303ba50cc0688e35
shivarkzs <82657136+shivarkzs@users.noreply.github.com>
2021-08-26 16:20:45 +0530

    msqlip

--- src/user/UserDashboard/UserDashboard.js
+++ src/user/UserDashboard/UserDashboard.js
@@ -10,7 +10,7 @@ import axios from 'axios';
 import SearchBar from '../../components/UI/SearchBar/SearchBar';
 import TableTemplate from '../../components/Tables/TableTemplate';
 
-const url = 'http://3.88.183.94:8080/api/employees';
+const url = 'http://172.31.93.73:8080/api/employees';
 
 class UserDashboard extends React.Component {
   state = {
